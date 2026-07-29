c7bb5a8594d826b21e0d04090d1401b64931bd9c
Sok Ratanak Vichea <ratanakvichea.sok26@kit.edu.kh>
2026-07-29 16:38:29 +0700

    Delete JS directory

--- JS/1-placement/script.js
+++
@@ -1,5 +0,0 @@
-function hideElement(){
-    let text = (document.getElementById('textId').textContent = "New Hello Show ")
-    return text;
-
-}
--- JS/BasicJS/array.js
+++
@@ -1,2 +0,0 @@
-const List_num = [1,2,3,4,5,6,7];
-console.log(List_num)
